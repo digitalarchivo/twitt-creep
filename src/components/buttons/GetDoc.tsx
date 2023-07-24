ab6d5c43a7337398192ce01a36a7e09c0088d189
Kevin Busch <kevinbusch@comcast.net>
2023-07-24 14:55:39 -0400

    loading modal

--- src/components/buttons/GetDoc.tsx
+++ src/components/buttons/GetDoc.tsx
@@ -1,8 +1,29 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-
+import Modal from 'react-modal';
 interface Props {
 }
+
+const customStyles = {
+  overlay: {
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      zIndex: 1000,
+  },
+  content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      backgroundColor: 'transparent',
+      border: 'none',
+      bottom: 'auto',
+      transform: 'translate(-50%, -50%)',
+  },
+  AnimationEffect: {
+      enter: 'ease-out',
+      exit: 'ease-in',
+  },
+
+};
 const GetDoc: React.FC<Props> = () => {
   const handleAddAll = () => {
     console.log('accounts to print',JSON.stringify(accounts));
@@ -34,7 +55,23 @@ if (!ISSERVER) {
 }
   const [accounts, setAccounts] = useState<string[] | []>(accounts1? accounts1: []);
 
+  const [modalIsOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  function openModal() {
+      setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      //   subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+      setIsLoading(prev => false);
+      setIsOpen(false);
+
+  }
   useEffect(() => {
     const handleStorageChange = () => {
 
@@ -57,6 +94,26 @@ if (!ISSERVER) {
         <div className='text-white text-lg'>You are currently saving {accounts.length} accounts</div>
         <button onClick={handleAddAll} className='p-x4 bg-amber-400 rounded-3xl hover:scale-110'>Add To List</button>
         <div className={`${isLoading?'absolute flex h-[90vh] w-[70-vw] opacity-50 bg-slate-700 text-center':'hidden'}`}>Adding to list........</div>
+        <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                ariaHideApp={false}
+                style={customStyles}
+                contentLabel="Remove Collateral Modal"
+            >
+                <div className={`border-2 border-purple-800 p-12 flex flex-col text-blue-400 rounded-xl inside-box ${isLoading&& 'shadow-xl shadow-red-700'}`}>
+                    <div className='text-center my-2'>
+                        {isLoading && (
+                            <div className='animate-pulse'>
+                                <p className='text-xl'>Deleting Monitored Account from DB</p>
+                                <button  className='bg-purple-600 p-3 rounded-2xl mx-8 mt-6 hover:scale-125 text-gray-500'>------------------</button>
+                                <button  className='bg-red-600 p-3 rounded-2xl mx-8 mt-6 hover:scale-125 text-gray-500'>----------------------------------------------------------</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </Modal>
       </div>
     )
   }
