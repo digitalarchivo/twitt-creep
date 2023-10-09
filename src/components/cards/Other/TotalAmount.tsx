9396f73ddd3e738961f572eaa20b4c5149cf19d1
Andy <andrewVB2012@protonmail.com>
2023-10-09 09:15:59 -0400

    loading state and fetch for total amount

--- src/components/cards/Other/TotalAmount.tsx
+++ src/components/cards/Other/TotalAmount.tsx
@@ -1,11 +1,30 @@
+'use client'
+import { getAllProccessed } from "@/components/utils/supabase";
+import { use, useEffect, useState } from "react";
 
 // @ts-ignore
 const TotalAmount = (amountProccessed ) => {
-    const totalAmt = amountProccessed.totalAmount.toLocaleString('en-US')
+  const [procc,setProcc] = useState<any>(amountProccessed.totalAmount.toLocaleString('en-US'));
+  const [isLoading,setIsLoading]= useState(true);
+    const getData = async () => {
+        const res = await getAllProccessed();
+        setProcc(res.totalAmount.toLocaleString('en-US'));
+        setIsLoading(false);
+    }
+    useEffect(() => {
+      getData();
+      return () => {
+          // here you can clean the effect in case the component gets unmonth before the async function ends
+      }
+  }, [])
     
+  if (isLoading){
+    return  <h3 className='text-purple-600 mt-4 text-4xl text-center'>Total Scraped:<span className="text-purple-600 font-bold font-serif animate-pulse">0000</span></h3>
+
+  }
 
   return amountProccessed?(
-    <h3 className='text-purple-600 mt-4 text-4xl text-center'>Total Scraped: <span className="text-purple-600 font-bold font-serif">{totalAmt}</span></h3>
+    <h3 className='text-purple-600 mt-4 text-4xl text-center'>Total Scraped: <span className="text-purple-600 font-bold font-serif">{procc}</span></h3>
   ):null;
 };
 
