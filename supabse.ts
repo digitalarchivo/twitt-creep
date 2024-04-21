49260d5a3f038e825060c31711d64013eaedd036
digitalarchivo <161184382+digitalarchivo@users.noreply.github.com>
2024-04-21 00:24:30 -0500

    Update supabse.ts

--- supabse.ts
+++ supabse.ts
@@ -1,7 +1,7 @@
 // supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://utvsxgfogcixgkztnvxo.supabase.co'
+const SUPABASE_URL = 'https://fliwnbugdopofaskxaep.supabase.co'
 const SUPABASE_PUBLIC_KEY = process.env.NEXT_PUBLIC_SUPABASE;
 
 // @ts-ignore
